7c1f86375724b62dd34c7078e67807dd099d5084
Matheus Quost <matheusquost@gmail.com>
2021-10-12 21:33:48 -0300

    a04 - lista de compras

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,57 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public lista = [];
+  public nome;
+
+  constructor(public alertCtrl: AlertController, public toastCtrl: ToastController) { }
+
+  adicionar() {
+    if (this.nome == undefined) {
+      this.exibeAlert('O campo não pode ser vazio')
+    } else {
+      let item = this.nome;
+      const index = this.getIndex(item)
+      if (index == -1) {
+        this.lista.push(item.toUpperCase());
+        this.exibeToast('"' + item + '" adicionado na lista.');
+        this.nome = undefined
+      } else {
+        this.exibeAlert("'"+item+"' já se encontra na lista.")
+      }
+    }
+  }
+
+  remover = function (item) {
+    const index = this.getIndex(item)
+    this.lista.splice(index, 1);
+    this.exibeToast("'" + item + "' removido da lista.");
+  };
+
+  getIndex = function (item) {
+    return this.lista.indexOf(item.toUpperCase());;
+  }
+
+  async exibeAlert(msg) {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Atenção',
+      message: msg,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async exibeToast(msg) {
+    const toast = await this.toastCtrl.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
 
 }
